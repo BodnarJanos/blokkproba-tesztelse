ba8efbc257cb39c05e2d3e9a8fa464656cc31600
BodnarJanos <bodnar.janos@hotmail.com>
2023-12-22 18:01:05 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 namespace blokkproba{
     //%blockId = blokkprobaxor
     //%block = "xor $a $b"
-    //% color=5 weight=100 icon"\uf1ec" block="Basic Blocks"
+    //% color=5 weight=100 icon="\uf1ec" block="Basic Blocks"
     export function xor(a:number, b:number): number{
         return a^b;
     }
