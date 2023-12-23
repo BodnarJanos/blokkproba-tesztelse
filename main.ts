5ad0247593f9c95cac84253335f3b4e5541573bb
BodnarJanos <bodnar.janos@hotmail.com>
2023-12-23 14:41:22 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace blokkproba{
     //%blockId = blokkprobaxor
-    //%block = "kizarovagy '|' $a $b"
+    //%block="kizarovagy '|' $a $b"
     //% color=5 weight=100 icon="\uf1ec"
     //% groups=['Led']
     export function xor(a:number, b:number): number{
