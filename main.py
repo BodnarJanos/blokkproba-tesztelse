e1ad0c8bb8dd301ed8ae5cfbc73ab977ca1e59dc
BodnarJanos <bodnar.janos@hotmail.com>
2023-12-25 09:05:33 +0100

    pxt.json, main.ts, main.py frissítése

---
+++ main.py
@@ -0,0 +1,11 @@
+@namespace
+class blokkproba:
+    # %blockId = blokkprobaxor
+    # %block="kizarovagy '|' $a $b"
+    # % color=5 weight=100 icon="\uf1ec"
+    # % groups=['Led']
+    # % a.min=10 a.max=100 a.defl=15
+    def xor(a: number, b: number):
+        return a ^ b
+
+    
--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
-namespace blokkproba{
-    //%blockId = blokkprobaxor
-    //%block="kizarovagy '|' $a $b"
-    //% color=5 weight=100 icon="\uf1ec"
-    //% groups=['Led']
-    //% a.min=10 a.max=100 a.defl=15
-    export function xor(a:number, b:number): number{
-        return a^b;
+namespace blokkproba {
+    //  %blockId = blokkprobaxor
+    //  %block="kizarovagy '|' $a $b"
+    //  % color=5 weight=100 icon="\uf1ec"
+    //  % groups=['Led']
+    //  % a.min=10 a.max=100 a.defl=15
+    export function xor(a: number, b: number): number {
+        return a ^ b
     }
+    
+    //% blockId = "array_pop_statement" block="remove last value from $list"
+    //% blockAliasFor=Array.pop"
+    export function _popStatement(): void;
+   
+
 }
+
